ce1a05ae03259c53c2050234bc77bc283a68b10e
Pickle <chaopangchiu@gmail.com>
2024-12-17 11:37:52 +0800

    check if hello in text message

--- app.py
+++ app.py
@@ -53,14 +53,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
+    if "哈囉" in event.message.text:
+        with ApiClient(configuration) as api_client:
+            line_bot_api = MessagingApi(api_client)
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text="請勿哈囉！")]
+                )
             )
-        )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
