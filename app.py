53471e9dc729a05e5d2a39cc6cfaef37cf34bd99
Pickle <chaopangchiu@gmail.com>
2024-12-27 17:13:12 +0800

    remove the Google Sheets API and its functions

--- app.py
+++ app.py
@@ -25,20 +25,6 @@ from linebot.v3.webhooks import (
     TextMessageContent
 )
 
-from google.auth.transport.requests import Request
-from google.oauth2 import service_account
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file credentials.json.
-SERVICE_ACCOUNT_FILE = "credentials.json"
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-
-# The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = "1foihGnfiA9n8jW-f0kxvgU6KyLrZd6Ftshw81knMOIU"
-RANGE_NAME = "Sheet1!A2:C"
-
 app = Flask(__name__)
 
 # Load .env file
@@ -50,6 +36,18 @@ channel_secret = os.getenv("CHANNEL_SECRET")
 configuration = Configuration(access_token=channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+KEYWORD_HELLO = "哈囉"
+KEYWORD_XINXIN = "心心"
+KEYWORD_TAGALL = "@All"
+MESSAGE_HELLO = "哈囉哈囉"
+MESSAGE_FOCUS = "不好意思大家!我最近比較需要專注，我比較容易分心，有訊息強迫症，我先退出群組了~  有事情可以透過ig或是line找我，謝謝！愛你們"
+WARNING_MESSAGE_HELLO = "請勿哈囉！"
+WARNING_MESSAGE_TAGALL = "不好意思 可以不要隨便使用@All嗎"
+UID_ADAI = "U94caa77e789684671659c08bde60fce1"
+
+allowed_chars = r".*"
+pattern = allowed_chars.join(KEYWORD_HELLO)
+regex = rf"{pattern}"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -81,101 +79,17 @@ def get():
     }
     return jsonify(response)
 
-def get_values():
-    
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    
-    try:
-        service = build("sheets", "v4", credentials=creds)
-
-        result = (
-            service.spreadsheets()
-            .values()
-            .get(spreadsheetId=SPREADSHEET_ID,
-                 range=RANGE_NAME)
-            .execute()
-        )
-        rows = result.get("values", [])
-        return rows
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        return error
-    
-def append_values(values):
-
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-    try:
-        service = build("sheets", "v4", credentials=creds)
-
-        body = {"values": values}
-        result = (
-            service.spreadsheets()
-            .values()
-            .append(
-                spreadsheetId=SPREADSHEET_ID,
-                range=RANGE_NAME,
-                valueInputOption="USER_ENTERED",
-                body=body,
-            )
-            .execute()
-        )
-        return result
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        return error
-    
-def clear_table():
-    
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    
-    try:
-        service = build("sheets", "v4", credentials=creds)
-        
-        result = (
-            service.spreadsheets()
-            .values()
-            .clear(
-                spreadsheetId=SPREADSHEET_ID,
-                range=RANGE_NAME,
-            )
-            .execute()
-        )
-        return result
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        return error
-    
-keyword_hello = "哈囉"
-keyword_xinxin = "心心"
-keyword_atall = "@All"
-hello_message = "哈囉哈囉"
-hello_warning_message = "請勿哈囉！"
-atall_warning_message = "不好意思 可以不要隨便使用@All嗎"
-focus_message = "不好意思大家!我最近比較需要專注，我比較容易分心，有訊息強迫症，我先退出群組了~  有事情可以透過ig或是line找我，謝謝！愛你們"
-uid_adai = "U94caa77e789684671659c08bde60fce1"
-allowed_chars = r".*"
-pattern = allowed_chars.join(keyword_hello)
-regex = rf"{pattern}"
-
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_text_message(event):
     
-    len_row_array = len(get_values())
-    if len_row_array >= 50:
-        clear_table()
-        append_values([[event.source.user_id, event.message.id, event.message.text]])
-    else:
-        append_values([[event.source.user_id, event.message.id, event.message.text]])
-        
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         
-    if event.source.user_id == uid_adai:
+    if event.source.user_id == UID_ADAI:
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=hello_message)],
+                messages=[TextMessage(text=MESSAGE_HELLO)],
                 notification_disabled=True
             )
         )
@@ -183,61 +97,47 @@ def handle_text_message(event):
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=hello_warning_message)],
+                messages=[TextMessage(text=WARNING_MESSAGE_HELLO)],
                 notification_disabled=True
             )
         )
-    elif keyword_atall in event.message.text:
+    elif KEYWORD_TAGALL in event.message.text:
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=atall_warning_message)],
+                messages=[TextMessage(text=WARNING_MESSAGE_TAGALL)],
                 notification_disabled=True
             )
         )
-    elif keyword_xinxin in event.message.text:
+    elif KEYWORD_XINXIN in event.message.text:
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=focus_message)],
+                messages=[TextMessage(text=MESSAGE_FOCUS)],
                 notification_disabled=True
             )
         )
     else:
         pass
-    
+
 @handler.add(MessageEvent, message=StickerMessageContent)
 def handle_sticker_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         
-    if event.source.user_id == uid_adai:
+    if event.source.user_id == UID_ADAI:
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=hello_message)],
+                messages=[TextMessage(text=MESSAGE_HELLO)],
                 notification_disabled=True
             )
         )
     else:
         pass
-    
+
 @handler.add(UnsendEvent)
 def handle_unsend(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        
-    row_array = get_values()
-    for i in range(len(row_array)-1, -1, -1):
-        if row_array[i][1] == event.unsend.message_id:
-            line_bot_api.push_message_with_http_info(
-            PushMessageRequest(
-                to=event.source.group_id,
-                messages=[TextMessage(text=f"你是不是想要說：「{row_array[i][2]}」")]
-                )
-            )
-        else:
-            pass
-        
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
