5296d1599f787bec86189d96cd8840d8689f2386
Pickle <chaopangchiu@gmail.com>
2025-01-06 17:33:03 +0800

    move the constants to another file

---
+++ config.py
@@ -0,0 +1,9 @@
+KEYWORD_HELLO = "哈囉"
+KEYWORD_XINXIN = "心心"
+KEYWORD_TAGALL = "@All"
+MESSAGE_HELLO = "哈囉哈囉"
+MESSAGE_FOCUS = "不好意思大家!我最近比較需要專注，我比較容易分心，有訊息強迫症，我先退出群組了~  有事情可以透過ig或是line找我，謝謝！愛你們"
+WARNING_MESSAGE_HELLO = "請勿哈囉！"
+WARNING_MESSAGE_TAGALL = "不好意思 可以不要隨便使用@All嗎"
+UID_ADAI = "U94caa77e789684671659c08bde60fce1"
+MAX_MESSAGE_LENGTH = 500
